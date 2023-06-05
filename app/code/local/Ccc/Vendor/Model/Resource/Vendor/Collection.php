b0c464ee0e6236d996ba02db1408bfaca4592b51
mudra301199 <mudra.patel999@gmail.com>
2023-06-05 09:23:09 +0530

    category-module

--- app/code/local/Ccc/Vendor/Model/Resource/Vendor/Collection.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Ccc_Vendor_Model_Resource_Vendor_Collection extends Mage_Core_Model_Resource_Db_Collection_Abstract
-{
-	protected function _construct()
-	{
-		parent::_construct();
-		$this->_init('vendor/vendor');
-	}
-}
---
+++ app/code/local/Mp/Category/Helper/Category.php
@@ -0,0 +1,8 @@
+<?php
+class Mp_Category_Helper_Category extends Mage_Core_Helper_Abstract
+{
+	public function __construct()
+	{
+		
+	}
+}	
