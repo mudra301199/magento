c2aac18c0ef8418d79f72e78db2671f7fc558f17
mudra301199 <mudra.patel999@gmail.com>
2023-06-05 09:29:03 +0530

    product-module

--- app/code/local/Ccc/Vendor/Model/Vendor.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Ccc_Vendor_Model_Vendor extends Mage_Core_Model_Abstract
-{
-	public function _construct()
-	{
-		parent::_construct();
-		$this->_init('vendor/vendor');
-	}
-}
---
+++ app/code/local/Mp/Product/Block/Adminhtml/Product.php
@@ -0,0 +1,24 @@
+<?php
+class Mp_Product_Block_Adminhtml_Product extends Mage_Adminhtml_Block_Widget_Grid_Container
+{
+	public function __construct()
+    {
+        $this->_blockGroup = 'product';
+        $this->_controller = 'adminhtml_product';
+        $this->_headerText = Mage::helper('product')->__('Manage Products');
+
+        parent::__construct();
+
+        if ($this->_isAllowedAction('save')) {
+            $this->_updateButton('add', 'label', Mage::helper('product')->__('Add New Product'));
+        } else {
+            $this->_removeButton('add');
+        }
+
+    }
+
+    protected function _isAllowedAction($action)
+    {
+        return Mage::getSingleton('admin/session')->isAllowed('product/adminhtml_product/' . $action);
+    }   
+}
