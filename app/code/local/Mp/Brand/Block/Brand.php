4c9052b30125a226a47f92f885273291010a6cee
mudra301199 <mudra.patel999@gmail.com>
2023-06-09 16:27:48 +0530

    brand-module-upgrade-0.3.0

--- app/code/local/Mp/Brand/Block/Brand.php
+++ app/code/local/Mp/Brand/Block/Brand.php
@@ -5,6 +5,10 @@ class Mp_Brand_Block_Brand extends Mage_Core_Block_Template
 	function __construct()
 	{
 		parent::__construct();
-		
+	}
+
+	public function getBrands()
+	{
+		return Mage::getModel('brand/brand')->getCollection()->addOrder('sort_order','ASC');
 	}
 }
