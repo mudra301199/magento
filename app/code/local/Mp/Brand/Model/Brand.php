2605004a0703b9e2d4a2f53c38dae210614ff2fe
mudra301199 <mudra.patel999@gmail.com>
2023-06-12 11:41:12 +0530

    brand-module-upgrade-0.4.0

--- app/code/local/Mp/Brand/Model/Brand.php
+++ app/code/local/Mp/Brand/Model/Brand.php
@@ -1,41 +1,40 @@
 <?php
 class Mp_Brand_Model_Brand extends Mage_Core_Model_Abstract
 {
-	public function _construct()
-	{
-		parent::_construct();
-		$this->_init('brand/brand');
-	}
+    function __construct()
+    {
+        $this->_init('brand/brand');
+    }
 
-	public function saveImage($img, $path)
-	{
-		$uploader = new Varien_File_Uploader($img);
-        $uploader->setAllowedExtensions(['jpg', 'jpeg', 'webp', 'png']);
+    public function saveImage($img, $path)
+    {
+        $uploader = new Varien_File_Uploader($img);
+        $uploader->setAllowedExtensions(['jpg', 'jpeg', 'gif', 'png']);
         $uploader->setAllowRenameFiles(true);
         $uploader->setFilesDispersion(false);
         $uploader->save($path);
         $filePath = $path . DS . $uploader->getUploadedFileName();
         $this->$img = $uploader->getUploadedFileName();
         return $this;
-	}
+    }
 
-	public function saveRewriteUrlKey()
-	{
-		$brandId = $this->brand_id;
-		$brand = Mage::getModel('brand/brand')->load($brandId);
-		$urlKey = $this->url_key;
-		$rewriteUrl = 'brand/' . $urlKey;
-		$rewrite = Mage::getModel('core/url_rewrite')->getCollection()
-		    ->addFieldToFilter('request_path', $rewriteUrl)
-		    ->getFirstItem();
-		if (!$rewrite->getId()) {
-		    $rewrite->setStoreId(0) 
-		        ->setIdPath('brand/' . $brandId)
-		        ->setRequestPath($urlKey)
-		        ->setTargetPath('brand/index/view/id/' . $brandId)
-		        ->setIsSystem(0)
-		        ->save();
-		}
+    public function saveRewriteUrlKey()
+    {
+        $brandId = $this->brand_id;
+        $brand = Mage::getModel('brand/brand')->load($brandId);
+        $urlKey = $this->url_key;
+        $rewriteUrl = 'brand/' . $urlKey;
+        $rewrite = Mage::getModel('core/url_rewrite')->getCollection()
+            ->addFieldToFilter('request_path', $rewriteUrl)
+            ->getFirstItem();
+        if (!$rewrite->getId()) {
+            $rewrite->setStoreId(0) 
+                ->setIdPath('brand/' . $brandId)
+                ->setRequestPath($rewriteUrl)
+                ->setTargetPath('brand/index/view/id/' . $brandId)
+                ->setIsSystem(0)
+                ->save();
+        }
 
-	}
+    }
 }
