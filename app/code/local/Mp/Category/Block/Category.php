e3e2ce16b41f4311cbd10c8e89fddb47980de339
mudra301199 <mudra.patel999@gmail.com>
2023-06-12 16:25:40 +0530

    featured-category-0.1.0

--- app/code/local/Mp/Category/Block/Category.php
+++ app/code/local/Mp/Category/Block/Category.php
@@ -1,8 +1,28 @@
 <?php
 class Mp_category_Block_category extends Mage_Core_Block_Template
 {
-	public function __construct()
-	{
-		parent::__construct();
-	}
+	
+	public function getCategories()
+    {
+         $categories = Mage::getModel('catalog/category')
+            ->getCollection()
+            ->addAttributeToSelect('image')
+            ->addAttributeToSelect('name')
+            ->addAttributeToSelect('is_active')
+            ->addFieldToFilter('is_active', 1)
+            ->addIsActiveFilter();
+        return $categories;
+    }
+
+    public function getCategoryUrl($category)
+    {
+        $categoryId = $category->entity_id; 
+        $storeId = Mage::app()->getStore()->getId();
+        $categoryUrl = Mage::getModel('core/url_rewrite')
+            ->getResource()
+            ->getRequestPathByIdPath('category/' . $categoryId, $storeId);
+
+        return $categoryUrl;
+    }
 }
+?>
